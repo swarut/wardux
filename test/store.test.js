8af98c21299e602254249f5022d3de8050269b21
Warut Surapat <s.warut@gmail.com>
2018-02-19 23:59:57 +0900

    Update store test.

--- test/store.test.js
+++ test/store.test.js
@@ -3,5 +3,7 @@ import { createStore } from '../src/index'
 test('createStore returns new store', () => {
   let reducer = (state, action) => {}
   let store = createStore(reducer)
-  expect(store).toBeTruthy
+
+  expect(store).not.toBeNull();
+  expect(store).toBeDefined();
 });

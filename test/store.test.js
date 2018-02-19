e96a3ae470f70534bf2c90449a5752cb24d7079c
Warut Surapat <s.warut@gmail.com>
2018-02-20 03:37:14 +0900

    Add test for combineReducers.

--- test/store.test.js
+++ test/store.test.js
@@ -1,4 +1,4 @@
-import { createStore } from '../src/index'
+import { createStore, combineReducers } from '../src/index'
 
 let reducer = (state, action) => {
   switch (action.type) {
@@ -13,31 +13,44 @@ let reducer = (state, action) => {
       return state
   }
 }
+let reducer2 = (state, action) => {}
 let store = createStore(reducer)
 let storeCallback = () => {}
-let action = {type: "add", result: 20}
+let action = { type: "add", result: 20 }
 
-test('createStore returns new store', () => {
-  expect(store).not.toBeNull();
-  expect(store).toBeDefined();
+describe('createStore', () => {
+  test('createStore returns new store', () => {
+    expect(store).not.toBeNull();
+    expect(store).toBeDefined();
+  })
+
+  test('store can be subscribed', () => {
+    store.subscribe(storeCallback)
+    let cb = store.getSubscribers()
+    expect(cb).not.toBeNull
+    expect(cb).toBeInstanceOf(Array)
+    expect(cb.length).toBe(1)
+  })
+
+  test('store can return state', () => {
+    let state = store.getStates()
+    expect(state).toBeDefined
+    expect(state).toEqual({})
+  })
+
+  test('store dispatches action', () => {
+    store.dispatch(action)
+    let state = store.getStates()
+    expect(state).toEqual({ result: 20 })
+  })
 })
 
-test('store can be subscribed', () => {
-  store.subscribe(storeCallback)
-  let cb = store.getSubscribers()
-  expect(cb).not.toBeNull
-  expect(cb).toBeInstanceOf(Array)
-  expect(cb.length).toBe(1)
-})
-
-test('store can return state', () => {
-  let state = store.getStates()
-  expect(state).toBeDefined
-  expect(state).toEqual({})
-})
-
-test('store dispatches action', () => {
-  store.dispatch(action)
-  let state = store.getStates()
-  expect(state).toEqual({ result: 20})
+describe('combineReducers', () => {
+  test('combineReducers creats new reducer object', () => {
+    let reducers = combineReducers({ reducer, reducer2 })
+    let keys = Object.keys(reducers)
+    expect(keys.length).toBe(2)
+    expect(reducers[keys[0]]).toEqual(reducer)
+    expect(reducers[keys[1]]).toEqual(reducer2)
+  })
 })

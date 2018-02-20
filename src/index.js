72bcde6b30765b5357e7475d798c685b14a09230
Warut Surapat <s.warut@gmail.com>
2018-02-21 03:07:50 +0900

    Update store to support combined reducer.

--- src/index.js
+++ src/index.js
@@ -18,7 +18,16 @@ export const createStore = (reducer) => {
 
     dispatch: function(action) {
       if (this.reducer) {
-        this.states = this.reducer(this.states, action)
+        if (typeof this.reducer === 'function')
+          this.states = this.reducer(this.states, action)
+        else if (typeof this.reducer === 'object') {
+          let newStates = {}
+          Object.keys(this.reducer).forEach((key) => {
+            newStates[key] = this.reducer[key](this.states[key] || {}, action)
+          })
+          this.states = newStates
+        }
+
         this.subscribers.forEach((sub) => {
           sub(this.states)
         })
--- test/combineReducers.test.js
+++ test/combineReducers.test.js
@@ -13,13 +13,25 @@ let reducer = (state, action) => {
       return state
   }
 }
-let reducer2 = (state, action) => {}
+let reducer2 = (state = {}, action) => {
+  switch (action.type) {
+    case "remove":
+      let res =  {
+        ...state,
+        result: action.result
+      }
+      return res
+    default:
+      return state
+  }
+}
 
 let reducers = combineReducers({ reducer, reducer2 })
 
 let store = createStore(reducers)
 let storeCallback = () => {}
-let action = { type: "add", result: 20 }
+let action1 = { type: "add", result: 20 }
+let action2 = { type: "remove", result: 2 }
 
 describe('combineReducers', () => {
   test('combineReducers creats new reducer object', () => {
@@ -27,5 +39,20 @@ describe('combineReducers', () => {
     expect(keys.length).toBe(2)
     expect(reducers[keys[0]]).toEqual(reducer)
     expect(reducers[keys[1]]).toEqual(reducer2)
+  })
+
+  test('after dispatch, state should be constructed from reducer key', () => {
+    store.dispatch(action1)
+    let state = store.getStates()
+    expect(state).not.toBeNull();
+    expect(state).toBeDefined();
+    expect(state).toHaveProperty('reducer', { result: 20 })
+    expect(state).toHaveProperty('reducer2', {})
+
+    store.dispatch(action2)
+    let nextState = store.getStates()
+    console.log("next state", nextState)
+    expect(nextState).toHaveProperty('reducer', { result: 20 })
+    expect(nextState).toHaveProperty('reducer2', { result: 2 })
   })
 })
